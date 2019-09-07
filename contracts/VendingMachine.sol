3479359f188a7241d9dafdcc1620a5121c9ff831
postables <postables@rtradetechnologies.com>
2019-09-06 23:17:29 -0700

    contracts: provide vendor name during registration with machine

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -13,18 +13,21 @@ contract VendingMachine {
     // vendors selling items through the machine
     mapping(address => bool) public vendors;
     mapping(address => address) public vendorContracts;
+    mapping(string => address) public vendorNames;
 
     constructor(string memory _location, address _backend) public {
         location = _location;
         backend = _backend;
     }
 
-    function addVendor(address _vendorContract) public returns (bool) {
+    function addVendor(string memory _name, address _vendorContract) public returns (bool) {
         require(!vendors[msg.sender], "vendor must not be registered");
+        require(vendorNames[_name] == address(0), "name must not be taken");
         VendorManagementI vmI = VendorManagementI(_vendorContract);
         require(vmI.owner() == msg.sender, "caller must be vendor manager");
         vendors[msg.sender] = true;
         vendorContracts[msg.sender] = _vendorContract;
+        vendorNames[_name] = _vendorContract;
         return true;
     }
 

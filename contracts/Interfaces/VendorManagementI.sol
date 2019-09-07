a620e2adfac0aa34d54b08f796bf5dcadefbb1b7
postables <postables@rtradetechnologies.com>
2019-09-06 22:36:46 -0700

    contracts: enable vending machine, add interfaces

---
+++ contracts/Interfaces/VendorManagementI.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.10;
+pragma experimental ABIEncoderV2;
+
+interface VendorManagementI {
+  function products (string calldata) external view returns (string memory name, uint256 cost);
+  function soldAt (string calldata, string calldata) external view returns (bool);
+  function removeProductLocation (string calldata _name, string calldata _location) external returns (bool);
+  function addProductLocation (string calldata _name, string calldata _location) external returns (bool);
+  function owner () external view returns (address);
+  function id () external view returns (bytes32);
+  function registerProduct (string calldata _name, string[] calldata _locations, uint256 _cost) external returns (bool);
+}
--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -1,8 +1,19 @@
-pragma solidity 0.5.10;
+pragma solidity ^0.5.10;
 
 import "./SafeMath.sol";
+import "./Interfaces/VendorManagementI.sol";
 
 /// @title VendingMachine - Represents a single, physical vending machine
 contract VendingMachine {
     using SafeMath for uint256;
+
+    // vendors selling items through the machine
+    mapping(address => bool) public vendors;
+
+    function addVendor(address _vendorContract) public returns (bool) {
+        VendorManagementI vmI = VendorManagementI(_vendorContract);
+        require(vmI.owner() == msg.sender, "caller must be vendor manager");
+        vendors[msg.sender] = true;
+        return true;
+    }
 }
